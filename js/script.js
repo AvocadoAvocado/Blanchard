115ba7abf31a782cad82fe9c84ca438e54a743c5
anna nalimova <f4f3545@mail.ru>
2022-05-16 14:48:21 +0300

    swaiper-btn

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 let swiper = new Swiper('.swiper', {
   slidesPerView: 1,
   slidesPerGroup: 1,
-  spaceBetween: 0,
   loop: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  }
 })
+
