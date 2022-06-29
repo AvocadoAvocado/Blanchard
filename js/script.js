8f0d52306d261ea66cd38954666b20b1ee669d37
anna nalimova <f4f3545@mail.ru>
2022-06-29 18:09:11 +0300

    js search

--- js/script.js
+++ js/script.js
@@ -187,3 +187,30 @@ function init() {
 ymaps.ready(init);
 
 // search
+let buttonSearch = document.querySelector('.search');
+let wrapperSearch = document.querySelector('.header__header-search');
+let formSearch = document.querySelector('.header__header-form');
+let buttonSearchClose = document.querySelector('.header__search-close')
+
+buttonSearch.addEventListener('click', ()=> {
+  wrapperSearch.classList.add('header__header-search--position');
+  formSearch.classList.remove('header__header-form--invis');
+})
+
+buttonSearchClose.addEventListener('click', function (){
+  wrapperSearch.classList.remove('header__header-search--position');
+  formSearch.classList.add('header__header-form--invis');
+});
+
+/*const searchForm = document.querySelector('.header__search-close');
+
+searchOpen.addEventListener('click', ()=> {
+  searchForm.classList.remove('header__header-search--position')
+  searchOpen.classList.add('header__header-search')
+  console.log('Hello word!')
+})
+
+searchForm.addEventListener('blur', function(){
+  this.style.border = 'none';
+})
+*/
